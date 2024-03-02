e60353559b91e25dae9cbc6d4eddfc77a0f05c7b
HAnh20082003 <pkpkpkbinhchuannrtnhab@gmail.com>
2024-03-02 13:35:02 +0700

    Xây dựng các phương thức trong controller DangNhap và các View cho các phương thức đó

--- Controllers/DangNhapController.cs
+++ Controllers/DangNhapController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyKetQuaHocTap.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,45 @@ namespace QuanLyKetQuaHocTap.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: Login
-        public ActionResult Index()
+        public static db_QuanLyKetQuaHocTapEntities db = new db_QuanLyKetQuaHocTapEntities();
+        // GET: DangNhap
+        public static tb_TaiKhoan taiKhoan;
+        public ActionResult DangNhap()
         {
+            Session["TK"] = null;
             return View();
         }
+
+        public ActionResult KiemTraDangNhap(string userName, string password)
+        {
+            Session["ErrorWrongAccount"] = null;
+            taiKhoan = db.tb_TaiKhoan.SingleOrDefault(n => n.TenDN == userName && n.MatKhau == password && n.LoaiTaiKhoan != General.intTaiKhoanQT);
+            if (taiKhoan != null)
+            {
+                Session["TK"] = taiKhoan;
+                if (taiKhoan.LoaiTaiKhoan == General.intTaiKhoanGV)
+                {
+                    return RedirectToAction("TrangChu", "GiangVien");
+                }
+                return RedirectToAction("TrangChu", "SinhVien");
+            }
+            else
+            {
+                Session["ErrorWrongAccount"] = "**Lỗi: Tài khoản không tồn tại";
+            }
+            return RedirectToAction("DangNhap");
+        }
+
+        public ActionResult DangXuat()
+        {
+            bool gv = taiKhoan.LoaiTaiKhoan == General.intTaiKhoanGV;
+            Session["TK"] = null;
+            if (gv)
+            {
+                return RedirectToAction("TrangChu", "GiangVien");
+            }
+            return RedirectToAction("TrangChu", "SinhVien");
+        }
+
     }
 }
