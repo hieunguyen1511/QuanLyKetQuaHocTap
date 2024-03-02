2c2a5d472656a2f8af1997594e04ad475bffd49b
Nguyễn Hoàng Anh <161095215+HAnh20082003@users.noreply.github.com>
2024-03-02 13:42:59 +0700

    Update GiangVienController.cs

--- Controllers/GiangVienController.cs
+++ Controllers/GiangVienController.cs
@@ -9,32 +9,10 @@ namespace QuanLyKetQuaHocTap.Controllers
 {
     public class GiangVienController : Controller
     {
-        public static db_QuanLyKetQuaHocTapEntities db = new db_QuanLyKetQuaHocTapEntities();
-        public static tb_GiangVien gv;
         // GET: GiangVien
         public ActionResult Index()
         {
             return View();
-        }
-        public ActionResult DangNhap()
-        {
-            return View();
-        }
-        public ActionResult KiemTraDangNhap(string userName, string password)
-        {
-            Session["ErrorWrongAccount"] = null;
-            var tk = db.tb_TaiKhoan.SingleOrDefault(n => n.TenDN == userName && n.MatKhau == password && n.LoaiTaiKhoan == General.intTaiKhoanGV);
-            if (tk != null)
-            {
-                gv = db.tb_GiangVien.SingleOrDefault(n => n.ID_TaiKhoan == tk.ID);
-                Session["GV"] = gv;
-                return RedirectToAction("TrangChu", "GiangVien");
-            }
-            else
-            {
-                Session["ErrorWrongAccount"] = "**Lỗi: Tài khoản không tồn tại";
-            }
-            return RedirectToAction("DangNhap", "GiangVien");
         }
     }
 }
