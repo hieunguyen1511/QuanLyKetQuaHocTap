49b7f40d148cd75ec1674965a55fabe6c8e55927
LeLoc1 <161099658+LeLoc1@users.noreply.github.com>
2024-03-10 15:24:27 +0700

    Update SinhVienController.cs

--- Controllers/SinhVienController.cs
+++ Controllers/SinhVienController.cs
@@ -9,9 +9,47 @@ namespace QuanLyKetQuaHocTap.Controllers
     public class SinhVienController : Controller
     {
         // GET: SinhVien
-        public ActionResult Index()
+        public static tb_SinhVien sv;
+
+        public ActionResult TrangChu()
         {
-            return View();
+            if (Session["TK"] != null)
+            {
+                tb_TaiKhoan taiKhoan = (tb_TaiKhoan)Session["TK"];
+                if (taiKhoan.LoaiTaiKhoan == General.intTaiKhoanSV)
+                {
+                    sv = taiKhoan.tb_SinhVien.First();
+                }
+            }
+            return View(sv);
+        }
+
+        public ActionResult ThongTinCaNhan()
+        {
+            return View(sv);
+        }
+        public ActionResult HienThiThongTinCaNhan_Partial(int? id)
+        {
+            var sv = General.db.tb_SinhVien.Find(id);
+            return PartialView(sv);
+        }
+
+        public ActionResult CapNhatThongTinCaNhan(int? id, FormCollection collection)
+        {
+            string email = collection["email"];
+            string sdt = collection["sdt"];
+            DateTime ngaySinh = DateTime.Parse(collection["ngaySinh"]);
+            bool gioiTinh = collection["gioiTinh"] != General.strGioiTinhNu;
+            tb_SinhVien sv = General.db.tb_SinhVien.Find(id);
+            sv.Email = email;
+            sv.SDT = sdt;
+            sv.NgaySinh = ngaySinh;
+            sv.GioiTinh = gioiTinh;
+            General.db.Entry(sv).State = System.Data.Entity.EntityState.Modified;
+            General.db.SaveChanges();
+            Session["SV"] = sv;
+            SinhVienController.sv = sv;
+            return RedirectToAction("ThongTinCaNhan");
         }
     }
 }
