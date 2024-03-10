978dc40571dc7c59f82430b122b92bcba0e57713
LeLoc1 <161099658+LeLoc1@users.noreply.github.com>
2024-03-10 15:28:35 +0700

    Update LoginController.cs

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -9,9 +9,42 @@ namespace QuanLyKetQuaHocTap.Controllers
     public class LoginController : Controller
     {
         // GET: Login
-        public ActionResult Index()
+        public static tb_TaiKhoan taiKhoan;
+        public ActionResult DangNhap()
         {
+            Session["TK"] = null;
             return View();
+        }
+
+        public ActionResult KiemTraDangNhap(string userName, string password)
+        {
+            Session["ErrorWrongAccount"] = null;
+            taiKhoan = General.db.tb_TaiKhoan.SingleOrDefault(n => n.TenDN == userName && n.MatKhau == password && n.LoaiTaiKhoan != General.intTaiKhoanQT);
+            if (taiKhoan != null)
+            {
+                Session["TK"] = taiKhoan;
+                if (taiKhoan.LoaiTaiKhoan == General.intTaiKhoanGV)
+                {
+                    return RedirectToAction("TrangChu", "GiangVien");
+                }
+                return RedirectToAction("TrangChu", "SinhVien");
+            }
+            else
+            {
+                Session["ErrorWrongAccount"] = "**Lỗi: Tài khoản không tồn tại";
+            }
+            return RedirectToAction("DangNhap");
+        }
+
+        public ActionResult DangXuat()
+        {
+            bool gv = taiKhoan.LoaiTaiKhoan == General.intTaiKhoanGV;
+            Session["TK"] = null;
+            if (gv)
+            {
+                return RedirectToAction("TrangChu", "GiangVien");
+            }
+            return RedirectToAction("TrangChu", "SinhVien");
         }
     }
 }
